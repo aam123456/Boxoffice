c0969874c77f469fbc4da84ca2016e0ad409837d
Akash <aakabd25@gmail.com>
2023-06-06 22:41:45 +0530

    module 6

--- src/pages/Show.jsx
+++ src/pages/Show.jsx
@@ -1,7 +1,10 @@
-import { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
+import { Link, useParams, useNavigate } from "react-router-dom";
 import { useQuery } from "@tanstack/react-query";
 import { getShowById } from "../api/tvmaze";
+import ShowMainData from "../components/Shows/ShowMainData";
+import Details from "../components/Shows/Details";
+import Seasons from "../components/Shows/Seasons";
+import Cast from "../components/Shows/Cast";
 
 const Show = () => {
   const { showId } = useParams();
@@ -9,39 +12,52 @@ const Show = () => {
   const { data: showData, error: showError } = useQuery({
     queryKey: ["show", showId],
     queryFn: () => getShowById(showId),
+    refetchOnWindowFocus: false,
   });
+
+  
 
   // const {showData,showError}= useShowById(showId)
   if (showError) {
     return <div> We have an error:{showError.message}</div>;
   }
   if (showData) {
-    return <div>Got Show data:{showData.name}</div>;
+    return (
+      <div>
+        {/* <button type="button" onClick={onGoBack}>Go back to home</button> */}
+        <Link to="/">Go back Home</Link>
+
+        <ShowMainData
+          image={showData.image}
+          name={showData.name}
+          rating={showData.rating}
+          summary={showData.summary}
+          genres={showData.genres}
+        />
+        <div>
+          <h2>Details</h2>
+          <Details
+            status={showData.status}
+            premiered={showData.premiered}
+            network={showData.network}
+          />
+        </div>
+
+        <div>
+          <h2>Seasons</h2>
+          <Seasons seasons={showData._embedded.seasons} />
+        </div>
+        <div>
+          <h2>Cast</h2>
+          <Cast cast={showData._embedded.cast} />
+        </div>
+      </div>
+    );
   }
 
   return <div>Show page for {showId}</div>;
 };
 export default Show;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 // const useShowById=(showId)=>{
 
